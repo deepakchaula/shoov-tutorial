b5c146bb6c6a6d08a87f7922ab09b3c2df8fdbac
Deepak Chaula <deepak@mediccreations.com>
2017-08-06 23:34:38 +0530

    Add baseline images for Chrome

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -41,7 +41,7 @@ var caps = selectedCaps ? capsConfig[selectedCaps] : undefined;
 var providerPrefix = process.env.PROVIDER_PREFIX ? process.env.PROVIDER_PREFIX + '-' : '';
 var testName = selectedCaps ? providerPrefix + selectedCaps : providerPrefix + 'default';
 
-var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://pages.shoov.io';
+var baseUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://dev.theoncallroom.com/app/webroot/design/Sub-Admin/';
 
 var resultsCallback = process.env.DEBUG ? console.log : shoovWebdrivercss.processResults;
 
